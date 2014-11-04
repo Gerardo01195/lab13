d3c0ca4ded73abe47068bf657d9b25261384d3a2
Gerardo01195 <gerardo416@yahoo.com>
2014-11-04 13:55:22 -0600

    70 pt. done

--- 70-100pt.py
+++ 70-100pt.py
@@ -27,8 +27,23 @@ class MyApp:
        	    self.up = Button(self.myContainer1)
        	    self.up.configure(text="up", background= "green")
        	    self.up.grid(row=0,column=0)
+       	    
+       	    self.left = Button(self.myContainer1)
+       	    self.left.configure(text="left", background= "yellow")
+       	    self.left.grid(row=0,column=1)
+       	    
+       	    self.right = Button(self.myContainer1)
+       	    self.right.configure(text="right", background= "cyan")
+       	    self.right.grid(row=0,column=2)
+       	    
+       	    self.down = Button(self.myContainer1)
+       	    self.down.configure(text="down", background= "purple")
+       	    self.down.grid(row=0,column=3)
        	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
+       	    self.left.bind("<Button-1>", self.leftClicked)
+       	    self.right.bind("<Button-1>", self.rightClicked)
+       	    self.down.bind("<Button-1>", self.downClicked)
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=RIGHT)
@@ -47,7 +62,18 @@ class MyApp:
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
-		
+	def leftClicked(self, event):   
+	     global oval
+	     global player
+	     drawpad.move(player,-20,0)	
+	def rightClicked(self, event):   
+	     global oval
+	     global player
+	     drawpad.move(player,20,0)
+	def downClicked(self, event):   
+	     global oval
+	     global player
+	     drawpad.move(player,0,20)	
 
 app = MyApp(root)
 root.mainloop()
